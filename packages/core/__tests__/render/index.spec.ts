bb11fd066c0d448e41c581623106115e000f1452
07akioni <07akioni2@gmail.com>
2020-04-02 00:04:21 +0800

    chore: typo fixes

--- packages/core/__tests__/render/index.spec.ts
+++ packages/core/__tests__/render/index.spec.ts
@@ -5,8 +5,8 @@ const {
   h
 } = CSSRender()
 
-describe('# parse selector path', () => {
-  it('should render as expected', () => {
+describe('#render', () => {
+  it('should render as expected(1)', () => {
     assertEqual(
       h('body', {
         margin: 0,
@@ -26,6 +26,25 @@ describe('# parse selector path', () => {
     }
     
     body.dark {
+      background-color: black;
+    }
+    
+    body .container {
+      width: 100%;
+    }`)
+  })
+  it('should render as expected(2)', () => {
+    assertEqual(
+      h('body', [
+        h('&.dark', {
+          backgroundColor: 'black'
+        }),
+        h('.container', {
+          width: '100%'
+        })
+      ]).render()
+      ,
+    `body.dark {
       background-color: black;
     }
     
