06c9d52b88e4574af9ec199dcfb57cfebc82ad28
07akioni <07akioni2@gmail.com>
2021-02-25 03:41:47 +0800

    build: eslint

---
+++ packages/css-render/.eslintrc.js
@@ -0,0 +1,5 @@
+require('@rushstack/eslint-config/patch/modern-module-resolution')
+
+module.exports = {
+  extends: ['@css-render/eslint-config/base']
+}
--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,5 +1,4 @@
 module.exports = {
-  root: true,
   overrides: [
     {
       files: ['*.ts'],
